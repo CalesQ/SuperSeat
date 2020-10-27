7b8fbf8ef37eabe44c57bb90bc37f184953dab77
CalesQ <1264251638@qq.com>
2020-10-27 14:30:10 +0800

    <debug>修复首次进入软件时，记住密码的问题

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -36,34 +36,41 @@
 				isSavePassword: false
 			}
 		},
-		
+
 		onLoad() {
-			
+
 			this.init();
-			
+
 		},
-		
+
 		methods: {
-			
+
+			getNowTimeYDSText() {
+				var date = new Date();
+				return (date.getHours().toString().length < 2 ? ("0" + date.getHours()) : date.getHours()) + ':' + ((date.getMinutes())
+					.toString()
+					.length < 2 ? "0" + (date.getMinutes()) : (date.getMinutes())) + ":" + (date.getSeconds().toString().length <
+					2 ? ("0" + date.getSeconds()) : date.getSeconds());
+			},
+
 			init() {
-				if(uni.getStorageSync("expire_time") > new Date().getTime()) {
+				if (uni.getStorageSync("expire_time") > new Date().getTime()) {
 					uni.switchTab({
 						url: '../my/my'
 					})
 					return;
 				}
-				
+
 				this.schoolId = uni.getStorageSync("school_id") != undefined ? uni.getStorageSync("school_id") : "";
-				
-				this.isSavePassword = uni.getStorageSync("is_save_pwd");
-				
-				if(this.isSavePassword) {
+
+				this.isSavePassword = uni.getStorageSync("is_save_pwd") != undefined ? uni.getStorageSync("is_save_pwd") : false;
+
+				if (this.isSavePassword) {
 					this.pwd = uni.getStorageSync("pwd") != undefined ? uni.getStorageSync("pwd") : "";
-				} 
+				}
 			},
-			
+
 			login() {
-				
 				var requestUrl = login_url + "?username=" + this.schoolId + "&password=" + this.pwd;
 				console.info(requestUrl);
 				uni.showLoading({
@@ -74,24 +81,24 @@
 			},
 
 			callback(res) {
-				
+
 				// 保存token
 				uni.setStorageSync('token', res.data.token);
 				uni.setStorageSync('school_id', this.schoolId);
 				uni.setStorageSync('pwd', this.pwd);
-				
+
 				var expireTime = new Date().getTime() + 10 * 60 * 1000;
 				uni.setStorageSync('expire_time', expireTime);
-				
+
 				uni.setStorageSync('is_save_pwd', this.isSavePassword);
-				
+
 				uni.hideLoading();
-				
+
 				uni.switchTab({
 					url: '../my/my'
 				})
 			},
-			
+
 			savePwdRadioHanble(e) {
 				this.isSavePassword = !this.isSavePassword;
 			}
